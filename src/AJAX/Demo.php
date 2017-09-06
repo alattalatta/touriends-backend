e658ec72121e4a66749d7f615aa2cc1459c17257
ENN <ENN@Local.posh>
2017-09-06 22:33:30 +0900

    AJAX\Demo: Reformat

--- src/AJAX/Demo.php
+++ src/AJAX/Demo.php
@@ -3,42 +3,42 @@
 namespace Touriends\Backend\AJAX;
 
 class Demo extends Base {
-	public static function init() {
-	    parent::registerAction('login', [__CLASS__, 'login']);
-	    parent::registerAction('register', [__CLASS__, 'register']);
-	    parent::registerAction('logout', [__CLASS__, 'logout']);
-	}
+    public static function init() {
+        parent::registerAction('login', [__CLASS__, 'login']);
+        parent::registerAction('register', [__CLASS__, 'register']);
+        parent::registerAction('logout', [__CLASS__, 'logout']);
+    }
 
-	public static function login() {
-	    $login = $_REQUEST['login'];
-	    $pwd = $_REQUEST['pwd'];
+    public static function login() {
+        $login = $_REQUEST['login'];
+        $pwd   = $_REQUEST['pwd'];
 
-	    if (is_user_logged_in()) {
-	        wp_logout();
+        if (is_user_logged_in()) {
+            wp_logout();
         }
-	    $user = wp_signon([
-            'user_login' => $login,
+        $user = wp_signon([
+            'user_login'    => $login,
             'user_password' => $pwd,
-            'remember' => true
+            'remember'      => true
         ], false);
 
-	    $success = is_wp_error($user) ? false : true;
-	    if (! $success) {
-	        die(json_encode([
-	            'success' => false,
+        $success = is_wp_error($user) ? false : true;
+        if (! $success) {
+            die(json_encode([
+                'success' => false,
                 'message' => '로그인 실패!'
             ]));
         }
 
-	    die(json_encode([
-	        'success' => $success,
-            'uid' => $user->ID
+        die(json_encode([
+            'success' => $success,
+            'uid'     => $user->ID
         ]));
     }
 
     public static function register() {
         $login = $_REQUEST['login'];
-        $pwd = $_REQUEST['pwd'];
+        $pwd   = $_REQUEST['pwd'];
 
         if (get_user_by('login', $login)) {
             die(json_encode([
@@ -47,18 +47,18 @@ class Demo extends Base {
         }
 
         wp_insert_user([
-            'user_pass' => $pwd,
+            'user_pass'  => $pwd,
             'user_login' => $login
         ]);
-	    die(json_encode([
-	        'success' => true
+        die(json_encode([
+            'success' => true
         ]));
     }
 
     public static function logout() {
-	    wp_logout();
-	    die(json_encode([
-	        'success' => true
+        wp_logout();
+        die(json_encode([
+            'success' => true
         ]));
     }
 }
