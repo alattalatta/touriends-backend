fd1e791d7f8e387c191efecca5dc43551913b22d
AhnSungJoo <potensj@gmail.com>
2017-09-25 11:49:26 +0900

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -6,8 +6,8 @@ class Main {
 	public $classes = [];
 	public function main() {
 		add_action('init', function() {
-			Post\Place::init();
-			AJAX\Demo::init();
+			//Post\Place::init();
+			//AJAX\Demo::init();
 			AJAX\Member::init();
 			AJAX\Tour::init();
 		});
