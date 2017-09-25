94084fe82ce6b2a700dc0688c2991d7286cb9c3f
jm9546 <jm9546@gmail.com>
2017-09-25 11:41:25 +0900

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -3,11 +3,13 @@
 namespace Touriends\Backend;
 
 class Main {
-    public $classes = [];
-
-    public function main() {
-        add_action('init', function () {
-            AJAX\Member::init();
-        });
-    }
+	public $classes = [];
+	public function main() {
+		add_action('init', function() {
+			Post\Place::init();
+            AJAX\Demo::init();
+			AJAX\Member::init();
+			AJAX\Tour::init();
+		});
+	}
 }
