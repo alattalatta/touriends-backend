3195755712a208a38c71dce0eed39675ec7ee0fc
ENN <ENN@Local.posh>
2017-09-25 23:14:09 +0900

    Register with image, set intro, get profile image

--- src/AJAX/Member.php
+++ src/AJAX/Member.php
@@ -8,6 +8,7 @@ class Member extends Base {
 		parent::registerAction('register', [__CLASS__, 'register']);
 		parent::registerAction('logout', [__CLASS__, 'logout']);
 		parent::registerAction('set_intro', [__CLASS__, 'setIntro']);
+		parent::registerAction('get_user_image', [__CLASS__, 'getUserImage']);
 	}
 
 	/**
@@ -35,13 +36,13 @@ class Member extends Base {
 				'error'   => 'login_duplicate'
 			]));
 		}
-		$userdata = array(
+		$userdata = [
 			'user_login'   => $login,
 			'user_pass'    => $pwd,
 			'display_name' => $name,
 			'user_email'   => $email,
 			'user_url'     => $website
-		);
+		];
 		$user_id  = wp_insert_user($userdata);
 		// 이미지 업데이트 (프사업로드)
 		// 이미지 업로드는 워드프레스 기본 업로더를 사용함
@@ -55,26 +56,14 @@ class Member extends Base {
 			]));
 		} else {
 			// 유저 메타에 외래키로 사진 ID 넣기
-			update_user_meta($user_id, 'image', $attachment_id);
+			update_user_meta($user_id, 'user_image', $attachment_id);
 		}
 		update_user_meta($user_id, 'user_birth', $_POST['birth']);
-		update_user_meta($user_id, 'user_image', $_POST['image']);
 		update_user_meta($user_id, 'user_nation', $_POST['nation']);
 		update_user_meta($user_id, 'user_gender', $_POST['gender']);
 
+		// 가입 후 자동 로그인
 		die(json_encode(self::signIn($login, $pwd)));
-	}
-
-	/**
-	 * 자기소개
-	 */
-	public static function setIntro() {
-		$intro = $_POST['intro'];
-		$uid = get_current_user_id();
-		$res = update_user_meta($uid, 'intro', $intro);
-		die(json_encode([
-			'success' => $res
-		]));
 	}
 
 	/**
@@ -98,7 +87,7 @@ class Member extends Base {
 		}
 		$user_id = get_current_user_id();
 		wp_logout();
-		$attachment_id = get_user_meta($user_id, 'image', true);
+		$attachment_id = get_user_meta($user_id, 'user_image', true);
 		if ($attachment_id) {
 			wp_delete_attachment($attachment_id, true);
 		}
@@ -108,6 +97,43 @@ class Member extends Base {
 		]));
 	}
 
+    /**
+     * 자기소개
+     */
+    public static function setIntro() {
+        $intro = $_POST['intro'];
+        $uid = get_current_user_id();
+        $res = update_user_meta($uid, 'intro', $intro);
+        die(json_encode([
+            'success' => $res
+        ]));
+    }
+
+    /**
+     * 프사 반환
+     */
+    public static function getUserImage() {
+        // attachment id
+        $aid = get_user_meta(get_current_user_id(), 'user_image', true);
+        if ($aid === '') {
+            die(json_encode([
+                'success' => false
+            ]));
+        }
+        $attachment_url = wp_get_attachment_image_url($aid);
+        die(json_encode([
+            'success' => true,
+            'url' => $attachment_url
+        ]));
+    }
+
+    /**
+     * 로그인/가입 공용 Sign in 메소드
+     * @param string $login ID
+     * @param string $pwd Password
+     *
+     * @return array
+     */
 	private static function signIn(string $login, string $pwd): array {
 		// 로그인 된 상태면 로그아웃 우선
 		if (is_user_logged_in()) {
