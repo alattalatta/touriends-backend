87c81336c402a7d09f20ed60432c0159c1943cd7
AhnSungJoo <potensj@gmail.com>
2017-09-30 13:07:58 +0900

    Update Recommend.php

--- src/AJAX/Recommend.php
+++ src/AJAX/Recommend.php
@@ -3,8 +3,8 @@ namespace Touriends\Backend\AJAX;
 class Recommend extends Base {
     public static function init() {
         parent::registerAction('getTheme', [__CLASS__, 'getTheme']);
-        parent::registerAction('getCalender', [__CLASS__, 'getCalender']);
         parent::registerAction('getLanguage', [__CLASS__, 'getLanguage']);
+        parent::registerAction('getCalender', [__CLASS__, 'getCalender']);        
     }
     /**
     * 투렌즈 추천 
@@ -14,9 +14,9 @@ class Recommend extends Base {
       $user_theme = get_user_meta($user_id,'user_theme',true);
   
     }    
-    public static function getLanguae() {
+    public static function getLanguage() {
         $user_id  = get_current_user_id();
-        $user_Language = get_user_meta($user_id,'user_language',true);
+        $user_language = get_user_meta($user_id,'user_language',true);
     }
     public static function  getCalender() {
         $user_id  = get_current_user_id();
@@ -24,4 +24,3 @@ class Recommend extends Base {
         $user_toDate =  get_user_meta($user_id,'user_toDate',true);
       }
 }
-
