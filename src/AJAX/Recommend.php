e587dfc634b56ac9554d7a309ed9897dd081fdee
AhnSungJoo <potensj@gmail.com>
2017-09-30 14:01:55 +0900

    Update Recommend.php

--- src/AJAX/Recommend.php
+++ src/AJAX/Recommend.php
@@ -7,20 +7,37 @@ class Recommend extends Base {
         parent::registerAction('getCalender', [__CLASS__, 'getCalender']);        
     }
     /**
-    * 투렌즈 추천 
+    * 테마 받아오기
     */
     public static function getTheme() {
-      $user_id  = get_current_user_id();
-      $user_theme = get_user_meta($user_id,'user_theme',true);
-      add_user_meta($user_id, 'user_test', $user_theme );
+        global $wpdb;
+        $user_id  = get_current_user_id();
+        $user_theme = get_user_meta($user_id,'user_theme',true);
+        add_user_meta($user_id, 'user_test', $user_theme );
+        $count =  $wpdb->get_var("SELECT count(DISTINCT user_id) FROM $wpdb->usermeta WHERE (meta_value = '$user_theme')");
+        $query = "SELECT DISTINCT user_id FROM $wpdb->usermeta WHERE (meta_value = '$user_theme')";
+        $result = $wpdb->get_results($query);
+        add_user_meta($user_id, 'user_test1', $result[0] );
+        add_user_meta($user_id, 'user_test2', $result[1] );
+        add_user_meta($user_id, 'user_count', $count );
     }    
+    /**
+    * 언어 받아오기
+    */
     public static function getLanguage() {
+        global $wpdb;
         $user_id  = get_current_user_id();
         $user_language = get_user_meta($user_id,'user_language',true);
+        $user_theme = $wpdb->get_var("SELECT user_id FROM $wpdb->usermeta WHERE (meta_value = $user_theme)");
     }
+    /**
+    * 일정 받아오기
+    */
     public static function  getCalender() {
+        global $wpdb;
         $user_id  = get_current_user_id();
         $user_fromDate =  get_user_meta($user_id,'user_fromDate',true);
         $user_toDate =  get_user_meta($user_id,'user_toDate',true);
+        $user_theme = $wpdb->get_var("SELECT user_id FROM $wpdb->usermeta WHERE (meta_value = $user_theme)");
       }
 }
