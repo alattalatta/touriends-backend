663bff99b2f249896e4c4cade6c37727ce60cc1d
AhnSungJoo <potensj@gmail.com>
2017-09-30 12:24:51 +0900

    Create Recommend.php

---
+++ src/AJAX/Recommend.php
@@ -0,0 +1,26 @@
+<?php
+namespace Touriends\Backend\AJAX;
+class Recommend extends Base {
+    public static function init() {
+        parent::registerAction('getTheme', [__CLASS__, 'getTheme']);
+        parent::registerAction('getCalender', [__CLASS__, 'getCalender']);
+        parent::registerAction('getLanguage', [__CLASS__, 'getLanguage']);
+    }
+    /**
+    * 투렌즈 추천 
+    */
+    public static function getTheme() {
+      $user_id  = get_current_user_id();
+      $user_theme = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE (meta_key = 'user_theme' AND user_id = $user_id)");
+      
+    }    
+    public static function getLanguae() {
+        $user_id  = get_current_user_id();
+        $user_Language = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE (meta_key = 'user_language' AND user_id = $user_id)");
+      }  
+    public static function  getCalender() {
+        $user_id  = get_current_user_id();
+        $user_fromDate = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE (meta_key = 'user_fromDate' AND user_id = $user_id)");
+        $user_toDate = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE (meta_key = 'user_toDate' AND user_id = $user_id)");
+      } 
+}
