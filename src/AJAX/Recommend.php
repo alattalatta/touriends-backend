7bffc44b00c670f74935674a8b8a2a31ad8d27c7
AhnSungJoo <potensj@gmail.com>
2017-09-30 12:43:45 +0900

    Update Recommend.php

--- src/AJAX/Recommend.php
+++ src/AJAX/Recommend.php
@@ -12,7 +12,8 @@ class Recommend extends Base {
     public static function getTheme() {
       $user_id  = get_current_user_id();
       $user_theme = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE (meta_key = 'user_theme' AND user_id = $user_id)");
-      
+      console_log($user_id);
+      console_log($user_theme);
     }    
     public static function getLanguae() {
         $user_id  = get_current_user_id();
@@ -22,5 +23,10 @@ class Recommend extends Base {
         $user_id  = get_current_user_id();
         $user_fromDate = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE (meta_key = 'user_fromDate' AND user_id = $user_id)");
         $user_toDate = $wpdb->get_var("SELECT meta_value FROM $wpdb->usermeta WHERE (meta_key = 'user_toDate' AND user_id = $user_id)");
-      } 
+      }
+      function console_log( $data ){
+        echo '<script>';
+        echo 'console.log('. json_encode( $data ) .')';
+        echo '</script>';
+      }
 }
