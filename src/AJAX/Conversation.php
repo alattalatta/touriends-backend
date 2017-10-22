a2fd4e6210b3181bac07f2720fd308bacdea4628
ENN <ENN@Local.posh>
2017-10-23 03:33:12 +0900

    Conversation query refactoring

--- src/AJAX/Conversation.php
+++ src/AJAX/Conversation.php
@@ -5,102 +5,76 @@ namespace Touriends\Backend\AJAX;
 use Touriends\Backend\User;
 
 class Conversation extends Base {
-    public static function init() {
-        parent::registerAction('readCheck', [__CLASS__, 'readCheck']);
-        parent::registerAction('sendMessage', [__CLASS__, 'sendMessage']);
-        parent::registerAction('conversation', [__CLASS__, 'conversation']);
-    }
+	public static function init() {
+		parent::registerAction('readCheck', [__CLASS__, 'readCheck']);
+		parent::registerAction('sendMessage', [__CLASS__, 'sendMessage']);
+		parent::registerAction('getConversation', [__CLASS__, 'getConversation']);
+	}
 
-    /**
-     * 메시지 읽음 체크
-     */
-    public static function readCheck() {
-        $user_id = User\Utility::getCurrentUser()->ID; // 현재 user_id
-        $you_id = $_POST['other'];
+	/**
+	 * 메시지 읽음 체크
+	 */
+	public static function readCheck() {
+		$user_id = User\Utility::getCurrentUser()->ID; // 현재 user_id
+		$you_id = $_POST['other'];
 
-        global $wpdb;
-        $table_name = $wpdb->prefix . 'message';
-       
-        $statement = <<<SQL
+		global $wpdb;
+		$table_name = $wpdb->prefix . 'message';
+
+		$statement = <<<SQL
 UPDATE $table_name SET read_ck = 1 WHERE re_id = $user_id AND se_id = $you_id
 SQL;
 
-        $wpdb->get_col($statement);
+		$wpdb->get_col($statement);
 
-        die(json_encode([
-            'success' => true
-        ]));
-    }
+		die(json_encode([
+			'success' => true
+		]));
+	}
 
-    /**
-     * 메세지 보내기(DB저장)
-     */
-    public static function sendMessage() {
-        $user_id = User\Utility::getCurrentUser()->ID; // 현재 user_id
-        // $you_id = $_POST['other'];
+	/**
+	 * 메세지 보내기(DB 저장)
+	 */
+	public static function sendMessage() {
+		global $wpdb;
+		$table_name = $wpdb->prefix . 'message';
+		$user_id = User\Utility::getCurrentUser()->ID; // 현재 user_id
 
-        global $wpdb;
+		$data = [
+			're_id'   => $_POST['other'],
+			'se_id'   => $user_id,
+			'note'    => $_POST['note'],
+			'read_ck' => 0
+		];
+		$wpdb->insert($table_name, $data);
 
-        if (isset($_POST['submit'])) {
-            $table_name = $wpdb->prefix . 'message';
+		die(json_encode([
+			'success' => true
+		]));
+	}
 
-            $data = array(
-                're_id' => $_POST['other'],
-                'se_id' => $user_id,
-                'note' => $_POST['note'],
-                'read_ck' => 0
-            );
-            $wpdb->insert($table_name, $data);
-        }
-        die(json_encode([
-            'success' => true
-        ]));
-    }
+	/**
+	 * 대화내용 불러오기
+	 */
+	public static function getConversation() {
+		global $wpdb;
+		$table_name = $wpdb->prefix . 'message';
 
-    /**
-     * 대화내용 불러오기
-     */
-    public static function conversation() {
-        $user_id = User\Utility::getCurrentUser()->ID; // 현재 user_id
-        $you_id = $_POST['other'];
+		$user_id = User\Utility::getCurrentUser()->ID; // 현재 user_id
+		$you_id = $_POST['other'];
 
-        global $wpdb;
-        $table_name = $wpdb->prefix . 'message';  
-        $messages = $wpdb->get_col("SELECT mid FROM $table_name WHERE (re_id = $user_id AND se_id = $you_id) OR (re_id = $you_id AND se_id = $user_id)");
+		$statement = <<<SQL
+SELECT mid, se_id, note, (se_id = $user_id) AS is_mine
+FROM $table_name
+WHERE
+	(re_id = $user_id AND se_id = $you_id) OR 
+	(re_id = $you_id AND se_id = $user_id)
+SQL;
+		$messages = $wpdb->get_results($statement, ARRAY_A);
 
-        date_default_timezone_set('Asia/Seoul');
-
-        $text = [];
-
-        foreach ($messages as $msg) {
-            $se_id = $wpdb->get_var("SELECT se_id FROM $table_name WHERE mid = $msg");
-            $time = $wpdb->get_var("SELECT sendTime FROM $table_name WHERE mid = $msg");
-            $note = $wpdb->get_var("SELECT note FROM $table_name WHERE mid = $msg");
-
-            if ($se_id == $user_id) {
-                $text[$msg] = [
-                    'time' => $time,
-                    'note' => $note,
-                    'sort' => 'right'
-                ];
-            } else {
-                $text[$msg] = [
-                    'time' => $time,
-                    'note' => $note,
-                    'sort' => 'left'
-                ];
-            }
-        }
-        
-        function cmp($a, $b) {
-            return strcmp($a["time"], $b["time"]);
-        }
-        usort($text[], "cmp");
-
-        die(json_encode([
-            'success' => true,
-            'sort' => $text,
-            'message' => $messages
-        ]));
-    }
+		die(json_encode([
+			'success'  => true,
+			'messages' => $messages
+		]));
+	}
 }
