652a015a9861c6d305d3a03984f24e4af128f358
jm9546 <jm9546@gmail.com>
2017-10-26 20:21:40 +0900

    Update Guid.php

--- src/AJAX/Guid.php
+++ src/AJAX/Guid.php
@@ -7,32 +7,32 @@ use Touriends\Backend\User;
 
 class Guid extends Base {
     public static function init() {
-        parent::registerAction('tourInfo', [__CLASS__, 'tourInfo']);
-//        parent::registerAction('defaultInfo', [__CLASS__, 'defaultInfo']);
+        parent::registerAction('tour_Info', [__CLASS__, 'tour_Info']);
+        parent::registerAction('detail_Info', [__CLASS__, 'detail_Info']);
+        parent::registerAction('common_Info', [__CLASS__, 'common_Info']);
     }
-
+    /**
+     * 관광정보 목록 조회
+     * content = 0 이면 관광지/문화/축제 전체 조회(서울 강남구 기본값)
+     * content = 1 이면 관광지 조회(서울 강남구 기본값)
+     * content = 2 이면 문화 전체 조회(서울 강남구 기본값)
+     * content = 3 이면 축제 전체 조회(서울 강남구 기본값)
+     */
     public static function tourInfo() {
 
         $content = $_POST['content'];
         $area = $_POST['area'];
         $data1 = null;
         $data2 = null;
-        if ($content == 0)
-        {
-            $data  = self::defaultInfo(76, $area);
+        if ($content == 0) {
+            $data = self::defaultInfo(76, $area);
             $data1 = self::defaultInfo(78, $area);
             $data2 = self::defaultInfo(85, $area);
-        }
-
-        else if($content == 1){
+        } else if ($content == 1) {
             $data = self::defaultInfo(76, $area);
-        }
-
-        else if($content == 2){
+        } else if ($content == 2) {
             $data = self::defaultInfo(78, $area);
-        }
-
-        else if($content == 3){
+        } else if ($content == 3) {
             $data = self::defaultInfo(85, $area);
         }
 
@@ -45,13 +45,13 @@ class Guid extends Base {
 
     }
 
-    public static function defaultInfo(int $content, int $area){
+    public static function defaultInfo(int $content, int $area) {
 
         $key = "ZdRsNK%2BUuytOnBip1bcGNL1P5pvkIQQQ3cJfcNlSgGEx%2BI6A8AftgREL7JZFmmi%2FyQZS3XY4irLiNc5BS%2FXIKA%3D%3D";
         $url = "http://api.visitkorea.or.kr/openapi/service/rest/EngService/areaBasedList";
-        $url .= "?ServiceKey=".$key;
+        $url .= "?ServiceKey=" . $key;
         $url .= "&cat1=A02";
-        $url .= "&contentTypeId=". $content;
+        $url .= "&contentTypeId=" . $content;
         $url .= "&areaCode=1";
         $url .= "&sigunguCode=" . $area;
         $url .= "&cat2=";
@@ -67,4 +67,61 @@ class Guid extends Base {
 
         return (array)$data;
     }
+    /**
+     * 소개 정보
+     */
+    public static function detail_Info() {
+
+        $contentId = $_POST['contentId']; // 1118680 : test
+
+        $key = "ZdRsNK%2BUuytOnBip1bcGNL1P5pvkIQQQ3cJfcNlSgGEx%2BI6A8AftgREL7JZFmmi%2FyQZS3XY4irLiNc5BS%2FXIKA%3D%3D";
+        $url = "http://api.visitkorea.or.kr/openapi/service/rest/EngService/areaBasedList";
+        $url .= "?ServiceKey=" . $key;
+        // $url .= "&contentTypeId=". $content;
+        $url .= "&contentId=" . $contentId;
+        $url .= "&pageNo=1";
+        $url .= "&numOfRows=1";
+        $url .= "&MobileApp=AppTest";
+        $url .= "&MobileOS=ETC";
+        $url .= "&arrange=A";
+        $response = file_get_contents($url);
+        $xml = simplexml_load_string($response) or die("Error: Cannot create object");
+        $detail = $xml->body->items;
+
+        die(json_encode([
+            'success' => true,
+            'detail' => $detail
+        ]));
+    }
+    /**
+     * 공통 정보
+     */
+    public static function common_Info() {
+
+        $contentId = $_POST['contentId']; // 1118680 : test
+
+        $key = "ZdRsNK%2BUuytOnBip1bcGNL1P5pvkIQQQ3cJfcNlSgGEx%2BI6A8AftgREL7JZFmmi%2FyQZS3XY4irLiNc5BS%2FXIKA%3D%3D";
+        $url = "http://api.visitkorea.or.kr/openapi/service/rest/EngService/areaBasedList";
+        $url .= "?ServiceKey=" . $key;
+        $url .= "&contentId=" . $contentId;
+        $url .= "&pageNo=1";
+        $url .= "&numOfRows=1";
+        $url .= "&MobileApp=AppTest";
+        $url .= "&MobileOS=ETC";
+        $url .= "&arrange=A";
+        $url .= "&firstImageYN=Y";
+        $url .= "&areacodeYN=Y";
+        $url .= "&addrinfoYN=Y";
+        $url .= "&overviewYN=Y";
+
+        $response = file_get_contents($url);
+        $xml = simplexml_load_string($response) or die("Error: Cannot create object");
+        $common = $xml->body->items;
+
+        die(json_encode([
+            'success' => true,
+            'detail' => $common
+        ]));
+    }
+
 }
